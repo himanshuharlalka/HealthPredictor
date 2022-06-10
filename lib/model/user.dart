4a006f1dd5b8f4e11957c9514c76df1473da8efc
devanshijhaveri24 <devanshijhaveri2001@gmail.com>
2022-06-10 18:00:31 +0530

    user.dart changes

--- lib/model/user.dart
+++ lib/model/user.dart
@@ -6,32 +6,52 @@ class UserDetails {
   String? bloodgroup;
   String? gender;
   List? ailments;
- String? get getName => this.name;
+  List? myopia;
+  List? chd;
+  List? cb;
+  List? pcod;
 
- set setName(String? name) => this.name = name;
+  String? get getName => this.name;
+
+  set setName(String? name) => this.name = name;
 
   get getHeight => this.height;
 
- set setHeight( height) => this.height = height;
+  set setHeight(height) => this.height = height;
 
   get getWeight => this.weight;
 
- set setWeight( weight) => this.weight = weight;
+  set setWeight(weight) => this.weight = weight;
 
   get getDob => this.dob;
 
- set setDob( dob) => this.dob = dob;
+  set setDob(dob) => this.dob = dob;
 
   get getBloodgroup => this.bloodgroup;
 
- set setBloodgroup( bloodgroup) => this.bloodgroup = bloodgroup;
+  set setBloodgroup(bloodgroup) => this.bloodgroup = bloodgroup;
 
   get getGender => this.gender;
 
- set setGender( gender) => this.gender = gender;
+  set setGender(gender) => this.gender = gender;
 
   get getAilments => this.ailments;
 
- set setAilments( ailments) => this.ailments = ailments;
+  set setAilments(ailments) => this.ailments = ailments;
 
+  get getMyopia => this.myopia;
+
+  set setMyopia(myopia) => this.myopia = myopia;
+
+  get getCb => this.cb;
+
+  set setCb(cb) => this.cb = cb;
+
+  get getChd => this.chd;
+
+  set setChd(chd) => this.chd = chd;
+
+  get getPcod => this.pcod;
+
+  set setPcod(pcod) => this.pcod = pcod;
 }
